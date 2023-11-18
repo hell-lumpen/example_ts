2501e712c9eff8faeba0cab90818a6bdf15fb341
Grubenko Maxim <mdgrubenko@maieducation>
2023-11-18 20:13:00 +0300

    test apiService

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import ApiService, {ApiResult, ApiServiceBuilder} from './apiService';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface Booking {
+    id: number
+    bookingGroupId: any
+    room: Room
+    owner: Owner
+    staff: any[]
+    groups: any[]
+    startTime: string
+    endTime: string
+    description: string
+    tag: Tag
+    createdAt: any
 }
 
-export default App;
+export interface Room {
+    roomId: number
+    roomName: string
+    capacity: number
+    hasComputers: boolean
+    hasProjector: boolean
+    isCathedral: boolean
+}
+
+export interface Owner {
+    userId: number
+    username: string
+    phoneNumber: string
+    fullName: string
+    password: string
+    isAccountLocked: boolean
+    role: string
+    enabled: boolean
+    accountNonLocked: boolean
+    authorities: Authority[]
+    credentialsNonExpired: boolean
+    accountNonExpired: boolean
+}
+
+export interface Authority {
+    authority: string
+}
+
+export interface Tag {
+    id: number
+    fullName: string
+    shortName: string
+    color: string
+}
+
+const MyComponent: React.FC = () => {
+    const [apiResult, setApiResult] = useState<ApiResult<Booking>>({
+        isLoading: false,
+        isError: false,
+        data: null,
+        error: undefined
+    });
+
+    // useEffect(() => {
+    //   (async () => {
+    //     const apiService = ApiServiceBuilder.builder('http://localhost:8080')
+    //         .withHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiQURNSU5JU1RSQVRPUiIsImZ1bGxOYW1lIjoi0JjRgNCx0LjRgtGB0LrQuNC5INCY0LvRjNGPINCh0LXRgNCz0LXQtdCy0LjRhyIsInN1YiI6InVzZXJuYW1lMyIsImlhdCI6MTcwMDMxMzUyNSwiZXhwIjoxNzAwOTE4MzI1fQ.9WV0PBzZvNnTk5DGYz2f7Oq6ZG-Sn1ImiQH4NNtizIs')
+    //         .build();
+    //
+    //     const response = await apiService.get<Booking>('/api/bookings/1');
+    //     setApiResult(response);
+    //   })();
+    // }, []);
+
+    const fetchData = async () => {
+        const apiService = ApiServiceBuilder.builder('http://localhost:8080')
+            .withHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiQURNSU5JU1RSQVRPUiIsImZ1bGxOYW1lIjoi0JjRgNCx0LjRgtGB0LrQuNC5INCY0LvRjNGPINCh0LXRgNCz0LXQtdCy0LjRhyIsInN1YiI6InVzZXJuYW1lMyIsImlhdCI6MTcwMDMxMzUyNSwiZXhwIjoxNzAwOTE4MzI1fQ.9WV0PBzZvNnTk5DGYz2f7Oq6ZG-Sn1ImiQH4NNtizIs')
+            .build();
+
+        const response = await apiService.get<Booking>('/api/bookings/12');
+        setApiResult(response);
+    };
+
+    return (
+        <div>
+            <button onClick={fetchData}>Fetch Data</button>
+            {apiResult.isLoading && <p>Loading...</p>}
+            {apiResult.isError && <p>Error loading data {JSON.stringify(apiResult.error)}</p>}
+            {apiResult.data && (
+                <div>
+                    <p>Data:</p>
+                    <p>
+                        {JSON.stringify(apiResult.data)}
+                    </p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default MyComponent;
---
+++ src/apiService.ts
@@ -0,0 +1,245 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+
+export interface Error {
+    type: string;
+    title: string;
+    status: number;
+    detail: string;
+    instance: string;
+    exception_description: string;
+}
+
+export interface ApiResult<T> {
+    isLoading: boolean;
+    isError: boolean;
+    data: T | null;
+    error?: Error;
+}
+
+class ApiService {
+    private readonly baseURL: string;
+    private headers: Record<string, string>[] = [];
+
+    private constructor(baseURL: string) {
+        this.baseURL = baseURL;
+    }
+
+    public static create(baseURL: string): ApiService {
+        return new ApiService(baseURL);
+    }
+
+    public withHeaders(headers: Record<string, string>[]): this {
+        this.headers.push(...headers);
+        return this;
+    }
+
+    private async request<T>(
+        config: AxiosRequestConfig
+    ): Promise<AxiosResponse<T>> {
+        try {
+            return await axios({
+                baseURL: this.baseURL,
+                ...config,
+            });
+        } catch (error: any) {
+            return Promise.reject({
+                response: error.response,
+                message: `API Error: ${error.message}`,
+            });
+        }
+    }
+
+    private getDefaultHeaders(): Record<string, string> {
+        const defaultHeaders: Record<string, string> = {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'http://localhost:3002',
+        };
+
+        const authToken = localStorage.getItem('token');
+        if (authToken) {
+            defaultHeaders['Authorization'] = `Bearer ${authToken}`;
+        }
+
+        return defaultHeaders;
+    }
+
+    async get<T>(
+        url: string,
+        params?: any,
+        additionalHeaders?: Record<string, string>[]
+    ): Promise<ApiResult<T>> {
+        const result: ApiResult<T> = {
+            isLoading: true,
+            isError: false,
+            data: null,
+            error: undefined,
+        };
+
+        try {
+            const headers = {
+                ...this.getDefaultHeaders(),
+                ...(additionalHeaders ? Object.assign({}, ...additionalHeaders) : {}),
+                ...this.getHeadersObject(),
+            };
+
+            const config: AxiosRequestConfig = {
+                method: 'get',
+                url,
+                params,
+                headers,
+            };
+
+            const response = await this.request<T>(config);
+            result.data = response.data;
+        } catch (error: any) {
+            result.isError = true;
+            result.error = error.response ? error.response.data : { detail: error.message };
+        } finally {
+            result.isLoading = false;
+        }
+        return result;
+    }
+
+    async post<T>(
+        url: string,
+        data?: any,
+        additionalHeaders?: Record<string, string>[]
+    ): Promise<ApiResult<T>> {
+        const result: ApiResult<T> = {
+            isLoading: true,
+            isError: false,
+            data: null,
+            error: undefined,
+        };
+
+        try {
+            const headers = {
+                ...this.getDefaultHeaders(),
+                ...(additionalHeaders ? Object.assign({}, ...additionalHeaders) : {}),
+                ...this.getHeadersObject(),
+            };
+
+            const config: AxiosRequestConfig = {
+                method: 'post',
+                url,
+                data,
+                headers,
+            };
+
+            const response = await this.request<T>(config);
+            result.data = response.data;
+        } catch (error: any) {
+            result.isError = true;
+            result.error = error.response ? error.response.data : { detail: error.message };
+        } finally {
+            result.isLoading = false;
+        }
+        return result;
+    }
+
+    async put<T>(
+        url: string,
+        data?: any,
+        additionalHeaders?: Record<string, string>[]
+    ): Promise<ApiResult<T>> {
+        const result: ApiResult<T> = {
+            isLoading: true,
+            isError: false,
+            data: null,
+            error: undefined,
+        };
+
+        try {
+            const headers = {
+                ...this.getDefaultHeaders(),
+                ...(additionalHeaders ? Object.assign({}, ...additionalHeaders) : {}),
+                ...this.getHeadersObject(),
+            };
+
+            const config: AxiosRequestConfig = {
+                method: 'put',
+                url,
+                data,
+                headers,
+            };
+
+            const response = await this.request<T>(config);
+            result.data = response.data;
+        } catch (error: any) {
+            result.isError = true;
+            result.error = error.response ? error.response.data : { detail: error.message };
+        } finally {
+            result.isLoading = false;
+        }
+        return result;
+    }
+
+    async delete<T>(
+        url: string,
+        additionalHeaders?: Record<string, string>[]
+    ): Promise<ApiResult<T>> {
+        const result: ApiResult<T> = {
+            isLoading: true,
+            isError: false,
+            data: null,
+            error: undefined,
+        };
+
+        try {
+            const headers = {
+                ...this.getDefaultHeaders(),
+                ...(additionalHeaders ? Object.assign({}, ...additionalHeaders) : {}),
+                ...this.getHeadersObject(),
+            };
+
+            const config: AxiosRequestConfig = {
+                method: 'delete',
+                url,
+                headers,
+            };
+
+            const response = await this.request<T>(config);
+            result.data = response.data;
+        } catch (error: any) {
+            result.isError = true;
+            result.error = error.response ? error.response.data : { detail: error.message };
+        } finally {
+            result.isLoading = false;
+        }
+        return result;
+    }
+
+    private getHeadersObject(): Record<string, string> {
+        return this.headers.reduce((acc, header) => {
+            const [name, value] = Object.entries(header)[0];
+            acc[name] = value;
+            return acc;
+        }, {} as Record<string, string>);
+    }
+}
+
+export class ApiServiceBuilder {
+    private readonly baseURL: string;
+    private headers: Record<string, string>[] = [];
+
+    private constructor(baseURL: string) {
+        this.baseURL = baseURL;
+    }
+
+    public static builder(baseURL: string): ApiServiceBuilder {
+        return new ApiServiceBuilder(baseURL);
+    }
+
+    public withHeader(name: string, value: string): this {
+        this.headers.push({ [name]: value });
+        return this;
+    }
+
+    public build(): ApiService {
+        const apiService = ApiService.create(this.baseURL);
+        apiService.withHeaders(this.headers);
+        return apiService;
+    }
+}
+
+export default ApiService;
